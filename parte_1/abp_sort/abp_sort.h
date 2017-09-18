36bef77199642820856da860a953ba2daf17a029
othonalberto <othonalberto@gmail.com>
2017-09-18 08:45:53 -0300

    enunciado do problema

--- parte_1/abp_sort/abp_sort.h
+++ parte_1/abp_sort/abp_sort.h
@@ -1,3 +1,8 @@
+/* Enunciado do problema: 
+ * Implemente o procedimento ordena(int v[], int n) que põe em ordem crescente o vetor v com n elementos.
+ * A ordenação deve ser efeita por intermédio de uma árvore binária de pesquisa.
+ */
+
 typedef struct NoABP {
     int chave;
     struct NoABP *esq;
