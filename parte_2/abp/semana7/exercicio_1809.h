1c993563d61f499e3ccb72af1727a86c15031370
othonalberto <othonalberto@gmail.com>
2017-09-20 13:14:28 -0300

    enunciado

--- parte_2/abp/semana7/exercicio_1809.h
+++ parte_2/abp/semana7/exercicio_1809.h
@@ -1,3 +1,10 @@
+/* ENUNCIADO DO EXERCÍCIO PREPARATÓRIO
+ *
+ * Seja o ponteiro raiz um ponteiro para o nó raiz da seguinte árvore binária de
+ * pesquisa (1 (2 (3)). Implemente um procedimento que balanceie a árvore
+ * mencionada. Faça as suposições necessárias.
+ */
+
 typedef struct TNoABP {
     int chave;
     struct TNoABP *esq;
